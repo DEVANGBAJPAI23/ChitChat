48ae96de6dcf94754eafb7bb7e306ff358e4a9fd
DEVANGBAJPAI23 <devangbajpai69@gmail.com>
2025-03-28 21:26:08 +0530

    changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,11 +6,11 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import { connectDB } from "./lib/db.js";
 import { app, server } from "./lib/socket.js";
-
+import path from "path";
 dotenv.config();
 
 const PORT = process.env.PORT || 5001; 
-
+const __dirname = path.resolve();
 // Increase body size limit
 app.use(express.json({ limit: "10mb" }));  // Increase limit for JSON payloads
 app.use(express.urlencoded({ limit: "10mb", extended: true })); // Increase limit for form data
@@ -23,7 +23,12 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes); 
 app.use("/api/messages", messageRoutes); 
-
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  })
+}
 server.listen(PORT, () => {
   console.log("Server is running on port :" + PORT);
   connectDB();
